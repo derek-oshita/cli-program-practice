cff2b3145522ba36d4640ab7f9e07cc26a3c700c
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2021-03-21 18:35:19 -0700

    added .toLowerCase() to ensure capital lettersin the data don't take priority.

--- bin/app.js
+++ bin/app.js
@@ -8,25 +8,25 @@ const boxen = require('boxen');
 const yargs = require('yargs'); 
 const axios = require('axios'); 
 
+
 // DATE VARIABLES ---------------------------------------------------------------------------------------------------------------------
 const today = new Date(); 
-const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
-// date number value according to the day of the month
+// get number code according to day of the week
 const todaysNumberCode = today.getDay(); 
 
+// create day, month, and year variables to format
 const dd = String(today.getDate()).padStart(2, '0'); 
-// month number value
 const mm = String(today.getMonth() + 1).padStart(2, '0'); 
-// get full year
 const yy = today.getFullYear();
 
+// interpolate the variables to format
 const todaysDate = `${mm}/${dd}/${yy}`
 const todaysTime = today.toLocaleTimeString(); 
+const todaysTime24Hours = today.getHours().toString()
 
 
 // CLI STYLING ------------------------------------------------------------------------------------------------------------------------
-// https://www.npmjs.com/package/boxen/v/4.0.0
 const greetingOptions = {
     padding: 1, 
     margin: 1, 
@@ -38,21 +38,11 @@ const greetingOptions = {
 const dateGreeting = chalk.white.bold(todaysDate); 
 const timeGreeting = chalk.white.bold(todaysTime)
 const greeting = chalk.white.bold('Welcome to Off The Grid, CLI!'); 
-const msgBox = boxen(greeting, greetingOptions); 
+const welcome = boxen(greeting, greetingOptions); 
+
 
 // CLI YARGS (ARGUMENTS) --------------------------------------------------------------------------------------------------------------
 // tutorial: https://dev.to/christopherkade/building-a-cli-with-yargs-ip8
-
-// const options = yargs   
-//     .usage('Usage: -n <name>')
-//     .option('n', {alias: 'name', describe: 'Your name', type: 'string', demandOption: false})
-//     .argv; 
-
-// const yargGreeting = `Hello, ${options.name}`
-
-// to get this to work, you run nodemon/node . -n someName otherwise it will return undefined
-// console.log(yargGreeting); 
-
 const options = yargs
     .usage('Usage: -p <page>')
     .option('p', {alias: 'page', describe: 'Page number', type:'number', demandOption: false})
@@ -60,27 +50,20 @@ const options = yargs
 
 
 // AXIOS FOR API ENDPOINT --------------------------------------------------------------------------------------------------------------
-
-// ALL TODAY 
+// Pass todaysNumberCode into URL to filter JSON for trucks that are open according to the day of the week.
 const url = `https://data.sfgov.org/resource/jjew-r69b.json?dayorder=${todaysNumberCode}`; 
 
-// ALL LIMIT 10
-// const url = `https://data.sfgov.org/resource/jjew-r69b.json?$limit=10&dayorder=${todaysNumberCode}`; 
-
-// ALL LIMIT 10 OFFSET 10
-// const url = `https://data.sfgov.org/resource/jjew-r69b.json?$limit=10&$offset=10&dayorder=${todaysNumberCode}`; 
-
+// API CALL 
 const getData = function (pageNumber) {
     axios.get(url)
     .then(res => {
-        // FILTER FOR OPEN TODAY AT CURRENT TIME
+        // FILTER FOR TRUCKS THAT ARE OPEN AT CURRENT TIME
         res = res.data.filter(truck => {
-            return today.getHours().toString() > truck.start24 
-            && today.getHours().toString() < truck.end24
+            return todaysTime24Hours > truck.start24 && todaysTime24Hours < truck.end24
         })
 
         // SORT BY APPLICANT NAME
-        res.sort((a,b) => a.applicant > b.applicant ? 1 : -1)
+        res.sort((a,b) => a.applicant.toLowerCase() > b.applicant.toLowerCase() ? 1 : -1)
 
         // MAP THROUGH RES ARRAY AND RETURN ONLY APPLICANT NAME AND TRUCK LOCATION
         res = res.map(truck => {
@@ -90,19 +73,21 @@ const getData = function (pageNumber) {
         // SHOW TOTAL PAGES
         const totalPages = Math.ceil(res.length / 10); 
 
-
         // IF NO PAGE NUMBER IS PROVIDED, SEND THE FIRST 10
         if (!pageNumber || pageNumber <= 1) {
             console.log(res.slice(0, 10))
-        } else if (pageNumber > totalPages) {
-            console.log('Nothing to see here!')
+        // IF THE USER PASSES A NUMBER GREATER THAN TOTAL AMOUNT OF PAGES, LOG ERROR MESSAGE
+        } else if (pageNumber > totalPages || typeof pageNumber !== 'number') {
+            console.error('Nothing to see here!')
+        // IF THE DATA IS VALID
         } else {
             const resultsArr = res.slice((pageNumber * 10) - 10, (pageNumber * 10)); 
             console.log(resultsArr.map(truck => truck))
         }
 
-
+        // SHOW CURRENT PAGE NUMBER / TOTAL PAGES
         console.log(`${pageNumber > totalPages ? `There are only ${totalPages} total pages..` : `Page: ${pageNumber || 1} / ${totalPages}`}`)
+        // SHOW TOTAL FOOD TRUCKS OPEN
         console.log('Total Food Trucks: ', res.length)
     })
     .catch(err => {
@@ -111,17 +96,17 @@ const getData = function (pageNumber) {
 }
 
 
+// OUTPUT ------------------------------------------------------------------------------------------------------------------------------
 
+// WELCOME 
+console.log(welcome); 
 
-
-// OUTPUT
-console.log(msgBox); 
+// DATE TABLE
 console.log('----------------------------------------------------')
 console.log(`||---- Today's Date: ${dateGreeting} at ${timeGreeting} ----||`); 
 console.log('----------------------------------------------------')
 
 // TRUCK DATA
-// getData(); 
 getData(options.page)
 
 
